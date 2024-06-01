ea68e5fed1a513a5296fc0589cf4b1a08b0bdc88
woo-lala <hyunj2034@naver.com>
2024-06-01 13:02:37 +0900

    feat : Initialize Project

---
+++ src/main/java/com/company/priceeverywhere/PriceeverywhereApplication.java
@@ -0,0 +1,13 @@
+package com.company.priceeverywhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceeverywhereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceeverywhereApplication.class, args);
+	}
+
+}
